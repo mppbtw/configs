f247512ffeb5bc5459cdace3411e49e2980897e4
mppbtw <mppbtw@protonmail.com>
2025-05-27 21:52:17 +0100

    ahh

--- dotfiles/nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,5 +1,3 @@
--- I like hoarding colorschemes and lazy loading them here
-
 return {
     {
         "ellisonleao/gruvbox.nvim",
@@ -40,5 +38,25 @@ return {
                 disable_background = false
             }
         end
-    }
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        event = "VeryLazy",
+        config = function()
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true},
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = true,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                theme = "dragon",              -- Load "wave" theme
+            })
+        end
+    },
 }
